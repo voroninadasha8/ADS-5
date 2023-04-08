9edbe60e5966919d63564050fb5ec55e623eec5b
voroninadasha8 <117026926+voroninadasha8@users.noreply.github.com>
2023-04-08 17:47:36 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,35 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, int size = 0>
 class TStack {
-  // добавьте код стека
+  private:
+    T arr[size] = {0};
+    int top;
+  public:
+    TStack() {
+      top = -1;
+    }
+    void Push(const T& value) {
+      if (!isFull()) {
+        arr[++top] = value;
+      }
+    }
+    T get() const {
+      return arr[top];
+    }
+    T pop() {
+      return arr[top--];
+    }
+    int GetTop() const {
+      return top;
+    }
+    bool isEmpty() const {
+      return top == -1;
+    }
+    bool isFull() const {
+      return top == size;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
