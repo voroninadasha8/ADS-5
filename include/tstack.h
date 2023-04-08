8f62ccab96fbcb00dfdd1791b9edeb250fdbd1a3
voroninadasha8 <117026926+voroninadasha8@users.noreply.github.com>
2023-04-08 14:29:12 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,35 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
-template<typename T, int size>
+template<typename type>
 class TStack {
-  // добавьте код стека
+ private:
+    static constexpr int kSize = 100;
+    type arr[kSize];
+    int top = -1;
+
+ public:
+    TStack() = default;
+    type get() const {
+        return arr[top];
+    }
+    bool isEmpty() const {
+        return top == -1;
+    }
+    bool isFull() const {
+        return top == kSize - 1;
+    }
+    void pop() {
+        if (!isEmpty()) {
+            --top;
+        }
+    }
+    void push(type item) {
+        if (!isFull()) {
+            arr[++top] = item;
+        }
+    }
 };
+
 
 #endif  // INCLUDE_TSTACK_H_
